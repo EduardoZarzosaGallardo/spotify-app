2a40d8fd79851cac11e9a498ddca7397bbd32ce8
andres <19170163@uttcampus.edu.mx>
2022-03-03 02:17:21 -0600

    Agregado pagina principal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,24 +3,83 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+        <div class="col-md-10">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2>Mi biblioteca</h2>
+                </div>
+                <div class="col-md-3">
+                    <div class="card">
+                        <a href="#">
+                            <img src="imagenes/album_intensive.jpg" class="card-img-top">
+                            <div class="card-body">
+                                <a class="link-cancion" href="#">
+                                    <h5 class="card-title">Cancion</h5>
+                                </a>
+                                <a class="link-artista" href="#">
+                                    <p class="card-text">Artista</p>
+                                </a>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card">
+                        <img src="imagenes/album_intensive.jpg" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Cancion</h5>
+                            <p class="card-text">Artista</p>
                         </div>
-                    @endif
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card">
+                        <img src="imagenes/album_intensive.jpg" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Cancion</h5>
+                            <p class="card-text">Artista</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="card">
+                        <img src="imagenes/album_intensive.jpg" class="card-img-top">
+                        <div class="card-body">
+                            <h5 class="card-title">Cancion</h5>
+                            <p class="card-text">Artista</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-4">
+                <div class="col-md-12">
+                    <h2>Explorar</h2>
+                </div>
+                <div class="col-md-3">
+                    <div class="categorias">
+                        <a href="#">
+                            <p class="categoria-descripcion">Pop</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="categorias">
+                        <p class="categoria-descripcion">Rock</p>
+                    </div>
+                </div>
 
-                    {{ __('You are logged in!') }}
+                <div class="col-md-3">
+                    <div class="categorias">
+                        <p class="categoria-descripcion">Jazz</p>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="categorias">
+                        <p class="categoria-descripcion">Electronica</p>
+                    </div>
                 </div>
             </div>
         </div>
-        <div id="app">
-<example-component></example-component>
-</div>
     </div>
 </div>
 
